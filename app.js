899d55f7be28900338ec26c6b686b7e66748e663
MashnoorJahan <mashnoorjahan2000@gmail.com>
2025-01-21 09:56:54 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require("cors")
 const app = express();
 
 const logger = require('./logger');
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 
 const router = require("./routes/fruits")
-
 
 app.get('/', (req, res) => {
   res.send('Hello Fruity!');
